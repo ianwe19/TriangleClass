0b24c5875e61938c6416f1178042c6a67d20fffb
shibiwhy <ian.welch19@icloud.com>
2022-10-02 21:51:10 -0400

    Add project files.

---
+++ TriangleClass/specification.h
@@ -0,0 +1,31 @@
+#ifndef __TRIANGLE__
+#define __TRIANGLE__
+
+#include <iostream>
+
+// CLASS TRIANGLE
+class Triangle {
+private:
+	float hypotenuseLength;
+	float adjacentLength;
+	float oppositeLength;
+	float area;
+	float perimeter;
+
+public:
+	// CONSTRUCTOR
+	Triangle();
+
+	// GETTERS
+	float getHypotenuse;
+	float getAdjacent;
+	float getOpposite;
+	float getArea;
+	float getPerimeter;
+
+	// SETTERS
+	bool setHypotenuse(float);
+	bool setAdjacent(float);
+	bool setOpposite(float);
+}; // end - Triangle
+#endif
