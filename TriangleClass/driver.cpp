0aab099d2b82e8f4ade4700927fd04c990dca14b
shibiwhy <ian.welch19@icloud.com>
2022-10-04 22:02:24 -0400

    add printTriangle()

--- TriangleClass/driver.cpp
+++ TriangleClass/driver.cpp
@@ -1,12 +1,32 @@
 #include <iostream>
+#include <iomanip> // for std::setw
 #include "specification.h"
+
+const int SPACELENGTH = 10;
+
+
+void printTriangle(Triangle);
 
 
 int main()
 {
-    Triangle triangle1;
+    Triangle triangle0;
 
-    
+    printTriangle(triangle0);
 
     return 0;
+}
+
+
+void printTriangle(Triangle triangle) { // absolutely not a good way of doing this
+    std::cout << "" << std::setw(SPACELENGTH) << "*\n";
+    std::cout << " " << std::setw(SPACELENGTH) << "**\n";
+    std::cout << "  " << std::setw(SPACELENGTH) << "* *\n";
+    std::cout << "   " << std::setw(SPACELENGTH) << "*  *\n";
+    std::cout << "    " << std::setw(SPACELENGTH) << "*   *\n";
+    std::cout << "     " << std::setw(SPACELENGTH) << "*    *\n";
+    std::cout << "      " << std::setw(SPACELENGTH) << "*     *\n";
+    std::cout << "       " << std::setw(SPACELENGTH) << "*      *\n";
+    std::cout << "        " << std::setw(SPACELENGTH) << "*       *\n";
+    std::cout << "        " << std::setw(SPACELENGTH) << "**********\n";
 }
