1eb73f6e6184bf5f433cfc65b0e6b9fdf3567679
shibiwhy <ian.welch19@icloud.com>
2022-10-23 11:44:24 -0400

    it works now

--- TriangleClass/driver.cpp
+++ TriangleClass/driver.cpp
@@ -13,7 +13,7 @@ int main()
     Triangle triangle0;
 
     triangle0.setAdjacent(21.4);
-    triangle0.setOpposite(2);
+    triangle0.setOpposite(2.53);
 
     printTriangle(triangle0);
 
@@ -24,25 +24,22 @@ int main()
 void printTriangle(Triangle triangle) {
     std::cout << std::fixed << std::setprecision(2) << std::noshowpoint;
 
-    std::cout << "" << std::setw(SPACELENGTH) << "*\n";
-    std::cout << " " << std::setw(SPACELENGTH) << "**\n";
-    std::cout << "  " << std::setw(SPACELENGTH) << "* *\n";
-    std::cout << "   " << std::setw(SPACELENGTH) << "*  *\n";
-    std::cout << "    " << std::setw(SPACELENGTH) << "*   *\n";
+    std::cout << "                        " << "*\n";
+    std::cout << "                        " << "**\n";
+    std::cout << "                        " << "* *\n";
+    std::cout << "                        " << "*  *\n";
+    std::cout << "                        " << "*   *\n";
 
+    std::cout << "Opposite Side =  " << std::setw(4) << triangle.getOpposite();
 
-    std::cout << std::setw(18) << "Opposite Side =  " << triangle.getOpposite(); // add setw
-    std::cout << "   *    *";
+    std::cout << "   " << "*    *" << "    Hypotenuse Side = " << triangle.getHypotenuse() << "\n";
 
+    std::cout << "                        " << "*     *\n";
+    std::cout << "                        " << "*      *\n";
+    std::cout << "                        " << "*       *\n";
+    std::cout << "                        " << "**********\n\n";
 
-    std::cout << "    Hypotenuse Side = " << triangle.getHypotenuse() << "\n";
-
-    std::cout << "      " << std::setw(SPACELENGTH) << "*     *\n";
-    std::cout << "       " << std::setw(SPACELENGTH) << "*      *\n";
-    std::cout << "        " << std::setw(SPACELENGTH) << "*       *\n";
-    std::cout << "          " << std::setw(SPACELENGTH) << "**********\n\n";
-
-    std::cout << std::setw(SPACELENGTH) << "                       Adjacent Side = " << triangle.getAdjacent() << "\n\n";
+    std::cout << "                       Adjacent Side = " << triangle.getAdjacent() << "\n\n";
 
     std::cout << "The area of the triangle is     : " << triangle.getArea() << "\n";
     std::cout << "The perimeter of the triangle is: " << triangle.getPerimeter() << "\n";
